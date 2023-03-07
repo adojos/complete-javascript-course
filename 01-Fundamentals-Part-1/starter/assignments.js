ba2e494380a1fec3b7922f6aeb042ecfa6975b94
Tushar Sharma <automationdojos.com>
2023-03-07 13:27:33 +0000

    coding challenge

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+let country = "India";
+let continent = "Asia";
+let population = 1416199923;
+
+console.log(
+  country +
+    " is situated in " +
+    continent +
+    " continent and has a population of " +
+    population +
+    " as of 2023."
+);
